355d93e23a07500c47bda9900ae01928cf042279
Metallicus Dev <>
2022-04-12 01:26:23 -0600

    :art: Split out exports

---
+++ assembly/chain.ts
@@ -0,0 +1,121 @@
+export { U128, U256 } from "as-chain";
+export { Float128 } from "as-chain"
+
+export { VarInt32, VarUint32, calcPackedVarUint32Length } from "as-chain";
+
+export { DBI64, PrimaryIterator } from "as-chain";
+export { IDX64 } from "as-chain";
+export { IDXF64 } from "as-chain";
+export { IDXF128 } from "as-chain";
+export { IDX128 } from "as-chain";
+export { IDX256 } from "as-chain";
+export { VariantValue } from "as-chain";
+
+export {
+    assert,
+    check,
+    currentTimePoint,
+    currentTime,
+    currentTimeMs,
+    currentTimeSec,
+} from "as-chain";
+
+export {
+    Microseconds,
+    TimePoint,
+    TimePointSec,
+    BlockTimestamp
+} from "as-chain"
+
+export {
+    prints,
+    printui,
+    print, printString, printArray, printHex, printi,
+    printI128,
+    printU128,
+    printsf,
+    printdf,
+    printqf,
+    printn,
+} from "as-chain";
+
+export {
+    IDXDB,
+    SecondaryType,
+    SecondaryValue,
+    SecondaryIterator,
+    newSecondaryValue_u64,
+    newSecondaryValue_U128,
+    newSecondaryValue_U256,
+    newSecondaryValue_f64,
+    newSecondaryValue_Float128,
+    getSecondaryValue_u64,
+    getSecondaryValue_U128,
+    getSecondaryValue_U256,
+    getSecondaryValue_f64,
+    getSecondaryValue_Float128,
+} from "as-chain";
+
+export { MultiIndex, MultiIndexValue, SAME_PAYER } from "as-chain";
+export { Singleton } from "as-chain";
+
+export {Contract, ActionWrapper, Table, InlineAction, Variant} from "as-chain"
+
+export {
+    getSender,
+    readActionData,
+    unpackActionData,
+    actionDataSize,
+    requireRecipient,
+    requireAuth,
+    hasAuth,
+    requireAuth2,
+    isAccount,
+    publicationTime,
+    currentReceiver
+} from "as-chain";
+
+export { Name, nameToSuffix } from "as-chain";
+export { Action, PermissionLevel } from "as-chain";
+export { Asset, ExtendedAsset, Symbol, ExtendedSymbol, isValid } from "as-chain";
+
+export {
+    sendDeferred,
+    cancelDeferred,
+    readTransaction,
+    transactionSize,    
+    taposBlockNum,
+    taposBlockPrefix,
+    transactionExpiration,
+    getAction,
+    getContextFreeData,
+    TransactionExtension,    
+    Transaction,
+} from "as-chain";
+
+export {
+    PublicKey,
+    Signature,
+    Checksum160,
+    Checksum256,
+    Checksum512,
+    recoverKey,
+    assertRecoverKey,
+
+    assertSha256,
+    assertSha1,
+    assertSha512,
+    assertRipemd160,
+    sha256,
+    sha1,
+    sha512,
+    ripemd160,
+} from "as-chain";
+
+export {
+    Packer,
+    Encoder,
+    Decoder,
+} from "as-chain";
+
+export { Utils } from "as-chain";
