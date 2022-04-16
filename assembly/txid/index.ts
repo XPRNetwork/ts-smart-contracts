e644d2ca7e725c0376d8b1b3d2f7a6708e9cec16
jafri <syedjafri97@gmail.com>
2022-04-16 01:39:24 -0600

    :sparkles: Add get transaction ID

---
+++ assembly/txid/index.ts
@@ -0,0 +1,9 @@
+import { Checksum256, transactionSize, readTransaction, check, sha256 } from "../chain";
+
+export function getTransactionId(): Checksum256 {
+    const size = transactionSize()
+    const buffer = new Array<u8>(<i32>size);
+    const read = readTransaction(buffer)
+    check(size == read, "readTransaction failed");
+    return sha256(buffer)
+}
--- examples/txid/txid.contract.ts
+++ examples/txid/txid.contract.ts
@@ -1,4 +1,4 @@
-import { Action, Name, requireAuth, Contract, transactionSize, print, Checksum256, readTransaction, check, sha256,  getAction, Table, TableStore } from 'proton-tsc'
+import { Name, getTransactionId, requireAuth, Contract, transactionSize, print, getAction, Table, TableStore } from 'proton-tsc'
 import { AccountKV, KV } from '../kv';
 
 @packer
@@ -22,7 +22,7 @@ export class TxIdContract extends Contract {
 
         const accountKv = new AccountKV(actor, [
             new KV("tx_size", `${transactionSize()}`),
-            new KV("tx_id", `${this.getTxid()}`)
+            new KV("tx_id", `${getTransactionId()}`)
         ])
         print(`${accountKv.values[0].value} | `)
         print(`${accountKv.values[1].value} | `)
@@ -31,26 +31,14 @@ export class TxIdContract extends Contract {
 
     @action("readaction")
     readaction(): void {
-        const rawAction: u8[] = getAction(1, 0)
-        const action = new Action()
-        action.unpack(rawAction)
+        const action = getAction(1, 0)
 
         const getSizeAndId = new GetSizeAndId()
         getSizeAndId.unpack(action.data)
 
-        print(`${getSizeAndId.actor}`)
-
         const accountKv = this.kvsTable.requireGet(getSizeAndId.actor.N, "could not find KV")
-        accountKv.values.push(new KV("action_size", `${rawAction.length}`))
+        accountKv.values.push(new KV("action_size", `${action.getSize()}`))
 
         this.kvsTable.update(accountKv, getSizeAndId.actor)
-    }
-
-    getTxid(): Checksum256 {
-        const size = transactionSize()
-        const buffer = new Array<u8>(<i32>size);
-        const read = readTransaction(buffer)
-        check(size == read, "readTransaction failed");
-        return sha256(buffer)
     }
 }
